5d9e95c1e33850492e21d11cc8b2dbd67b19d783
evansmosomi <evans.mosomi@ontariotechu.net>
2022-03-21 19:49:33 -0400

    Front end upload

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StudentForm } from "./components/StudentForm";
+import "./index.css";
+const App = () => {
+  return (
+    <div className="App">
+      <StudentForm />
+    </div>
+  );
+};
+
+export default App;
